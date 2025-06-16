c343d2ccdf6626a4fe4d069e4d87602d6167bf2a
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-06-16 12:17:58 +0200

    add new route: add new post to the website with get or post.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,37 @@ def index():
         blog_posts = json.load(file)
 
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Get data from the form
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        # Load existing data
+        with open('storage/storage.json', 'r') as file:
+            blog_posts = json.load(file)
+
+        # Create new post
+        new_post = {
+            'id': blog_posts[-1]['id'] + 1 if blog_posts else 1,
+            'author': author,
+            'title': title,
+            'content': content,
+        }
+        blog_posts.append(new_post)
+
+        with open('storage/storage.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        # Redirect home after submission
+        return render_template('index.html', posts=blog_posts)
+
+    # GET: Show form
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
