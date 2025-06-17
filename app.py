d0a8b422f0aecbcff862eefd7aa55fc8f8f1f19c
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-06-17 10:43:13 +0200

    clean up code: add dockstrings and functions to read and save data, clean up code according to PEP8

--- app.py
+++ app.py
@@ -5,17 +5,47 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+def load_posts():
+    """
+    loads the posts from the JSON database.
+    :return: the list of posts dictionary's.
+    """
+    try:
+        with open('storage/storage.json', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def save_posts(posts):
+    """ Saves new data to the JSON database."""
+    with open('storage/storage.json', 'w') as file:
+        json.dump(posts, file, indent=2)
+
+
+
 @app.route('/')
 def index():
+    """
+    The index page.
+    :return: the index page with all the blog posts.
+    """
     # add code here to fetch the job posts from a file
-    with open('storage/storage.json', 'r') as file:
-        blog_posts = json.load(file)
+    blog_posts = load_posts()
 
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """
+    The add page, adds posts to the blog.
+             GET: show the add page form.
+            POST: add a new post to the blog.
+    :return:
+            POST: redirect to index.html
+            GET: add.html form
+
+    """
     if request.method == 'POST':
         # Get data from the form
         author = request.form['author']
@@ -23,8 +53,7 @@ def add():
         content = request.form['content']
 
         # Load existing data
-        with open('storage/storage.json', 'r') as file:
-            blog_posts = json.load(file)
+        blog_posts = load_posts()
 
         # Create new post
         new_post = {
@@ -35,11 +64,10 @@ def add():
         }
         blog_posts.append(new_post)
 
-        with open('storage/storage.json', 'w') as file:
-            json.dump(blog_posts, file, indent=4)
+        save_posts(blog_posts)
 
         # Redirect home after submission
-        return render_template('index.html', posts=blog_posts)
+        return redirect(url_for('index'))
 
     # GET: Show form
     return render_template('add.html')
@@ -47,13 +75,17 @@ def add():
 
 @app.route('/delete/<post_id>', methods=['GET', 'POST'])
 def delete_post(post_id):
+    """
+    The delete page, deletes a post from the blog.
+    :param post_id: the id of the post do delete.
+    :return: redirect to index.html
+    """
     try:
         post_id = int(post_id)
     except ValueError:
         return render_template('index.html', message="Invalid post ID.")
 
-    with open('storage/storage.json', 'r') as file:
-        blog_posts = json.load(file)
+    blog_posts = load_posts()
 
     # Check if the post exists
     updated_posts = [post for post in blog_posts if post['id'] != post_id]
@@ -63,17 +95,22 @@ def delete_post(post_id):
         return render_template('index.html', posts=blog_posts, message=error_message)
 
     # Save the updated post list
-    with open('storage/storage.json', 'w') as file:
-        json.dump(updated_posts, file, indent=2)
+    save_posts(updated_posts)
 
     return redirect(url_for('index'))
 
 
 @app.route('/update/<post_id>', methods=['GET', 'POST'])
 def update_post(post_id):
+    """
+    The update page, updates a post from the blog.
+    :param post_id: the id of the post to update.
+    :return:
+            GET: show the update.html form.
+            POST: redirect index.html
+    """
     # Load all blog posts
-    with open('storage/storage.json', 'r') as file:
-        blog_posts = json.load(file)
+    blog_posts = load_posts()
 
     # Find the post by ID
     post = None
@@ -92,8 +129,7 @@ def update_post(post_id):
         post['content'] = request.form['content']
 
         # Save the updated list of posts
-        with open('storage/storage.json', 'w') as file:
-            json.dump(blog_posts, file, indent=2)
+        save_posts(blog_posts)
 
         return redirect(url_for('index'))
 
@@ -103,14 +139,18 @@ def update_post(post_id):
 
 @app.route('/like/<post_id>', methods=['GET', 'POST'])
 def like_post(post_id):
+    """
+    The like page, likes a post from the blog and counts the likes.
+    :param post_id: the id of the post to like.
+    :return: redirect ti index.html
+    """
     try:
         post_id = int(post_id)
     except ValueError:
         return render_template('index.html', message="Invalid post ID.")
 
     # Load all blog posts
-    with open('storage/storage.json', 'r') as file:
-        blog_posts = json.load(file)
+    blog_posts = load_posts()
 
     # Find the post by ID
     post_found = False
@@ -124,11 +164,10 @@ def like_post(post_id):
     if not post_found:
         return "Post not found", 404
 
-    with open('storage/storage.json', 'w') as file:
-        json.dump(blog_posts, file, indent=2)
+    save_posts(blog_posts)
 
     return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
